c304f5001f0ba4d52c22a65a5173412792a44f3a
Toshiki Tsuruga <toshturu113@gmail.com>
2018-09-07 09:58:43 +0900

    サードコミット

--- JankenGameConflict_05/src/janken/JankenController.java
+++ JankenGameConflict_05/src/janken/JankenController.java
@@ -10,6 +10,7 @@ public class JankenController {
      private  static final String[] hands = {
          "グー", "チョキ", "パー"
      };
+     private int score = 0;
 
       private Label cpuHandLabel;
       private Label myHandLabel;
@@ -53,6 +54,13 @@ public class JankenController {
      //じゃんけんの結果の取得
      private String getResult(int myHand, int cpuHand) {
          int r = (myHand - cpuHand + 3) % 3;
+         if(r == 2) {
+             score +=2;
+            } else if(r==1) {
+             score -=1;
+            } else {
+             score +=1;
+            }
 
          return (r == 2) ? "あなたの勝ち！" : ((r == 1) ? "あなたの負け！" : "あいこ！");
      }
@@ -61,5 +69,6 @@ public class JankenController {
          cpuHandLabel.setText("コンピュータの手: " + hands[cpuHand]);
          myHandLabel.setText("あなたの手: " + hands[myHand]);
          resultLabel.setText("結果: " + getResult(myHand, cpuHand));
+         scoreLabel.setText(" Score:"+score);
      }
 }
